e4804233cc359fb848d3780c89d4046dffdf8dca
A.K <let.it.be.1960@gmail.com>
2019-11-17 21:43:03 +0100

    logo added

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,36 @@
 <template>
-  <v-app id="app">
+  <v-app id="app" style="background: #37474F" >
     <div>
-      <v-app-bar height="65" dark>
+      <v-app-bar height="65" light>
 
-        <v-toolbar-title>Flocker</v-toolbar-title>
+        <img src="./assets/logo.png" height="65">
 
         <div class="flex-grow-1"></div>
 
-        <v-col cols="12" sm="6" md="3">
+        <v-col cols="12" sm="6" md="3" >
           <v-text-field
+            font-color="black"
             placeholder="Podaj wyszukiwaną frazę"
           ></v-text-field>
         </v-col>
 
         <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
+          <v-icon color="black">mdi-magnify</v-icon>
         </v-btn>
 
         <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
+          <v-icon color="black">mdi-heart</v-icon>
         </v-btn>
-
-
 
         <v-btn icon>
-          <v-icon>mdi-account</v-icon>
+          <v-icon color="black">mdi-account</v-icon>
         </v-btn>
 
 
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      </v-app-bar>
-      <v-container fluid>
-        <v-row justify="space-around">
+        <v-app-bar-nav-icon color="black"></v-app-bar-nav-icon>
+      </v-app-bar >
+      <v-container fluid >
+        <v-row jcolor="black"ustify="space-around">
         </v-row>
       </v-container>
     </div>
