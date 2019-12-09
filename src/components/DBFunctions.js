c6a3cc52858ee636d63fec8b587da8dc0b3fc1f5
244954 <mateusz_m_malecki@interia.pl>
2019-12-09 23:44:12 +0100

    asynchroniczne pobieranie z firestore

--- src/components/DBFunctions.js
+++ src/components/DBFunctions.js
@@ -15,7 +15,7 @@ function addUser(id,email) {
     db.collection('Users').doc(id).set(data);
 }
 
-// imie uzytkownika
+// ustal imie uzytkownika
 // wzorzec:
 // if (firebase.auth().currentUser)
 //   setUserName(firebase.auth().currentUser.uid,"Adam");
@@ -23,7 +23,7 @@ function setUserName(id,name) {
     db.collection('Users').doc(id).update({name: name});
 }
 
-// nazwisko uzytkownika
+// ustal nazwisko uzytkownika
 // wzorzec:
 // if (firebase.auth().currentUser)
 //   setUserLastName(firebase.auth().currentUser.uid,"Kowalski");
@@ -31,4 +31,62 @@ function setUserLastName(id,nazwisko) {
     db.collection('Users').doc(id).update({last_name: nazwisko});
 }
 
-export {addUser,setUserName,setUserLastName};
+// pobierz imie uzytkownika
+// wzorzec
+// if (firebase.auth().currentUser)
+//   getUserName(firebase.auth().currentUser.uid).then(function (output) {
+//           somestring = output;
+//   }
+function getUserName(id) {
+    let userRef = db.collection('Users').doc(id);
+    return userRef.get()
+        .then( function(doc) {
+            if (!doc.exists) {
+                // eslint-disable-next-line no-console
+                console.log('No such document!');
+                return "";
+            } else {
+                // eslint-disable-next-line no-console
+                console.log('Document data:',doc.data().name);
+                return doc.data().name;
+            }
+        })
+        .catch(err => {
+            // eslint-disable-next-line no-console
+            console.log('Error getting document', err);
+            return "";
+        });
+}
+// pobierz podroze uzytkownika
+// wzorzec
+// if (firebase.auth().currentUser)
+//   getTravels(firebase.auth().currentUser.uid).then(function (output) {
+//       somestring = output; // zwroci array ze stringami
+//   }
+function getTravels(id) {
+    let userRef = db.collection('Travel_Members');
+    return userRef.where('id_user', '==', id).get()
+        .then(snapshot => {
+            var travs = [];
+            if (snapshot.empty) {
+                // eslint-disable-next-line no-console
+                console.log('No matching documents.');
+                return travs;
+            }
+
+            snapshot.forEach(doc => {
+                // eslint-disable-next-line no-console
+                console.log(doc.id, '=>', doc.data(), typeof doc.data().id_travel);
+                travs.push(doc.data().id_travel);
+            });
+            return travs;
+        })
+        .catch(err => {
+            // eslint-disable-next-line no-console
+            console.log('Error getting documents', err);
+            return;
+        });
+}
+
+
+export {addUser,setUserName,setUserLastName,getUserName,getTravels};
